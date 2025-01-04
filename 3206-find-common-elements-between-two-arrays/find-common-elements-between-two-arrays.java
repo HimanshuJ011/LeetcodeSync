2793c5f8a0f55593b040b5e3932aceabd8db6583
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-04 10:57:33 +0530

    Time: 2 ms (96.71%) | Memory: 45.6 MB (37.35%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -19,27 +19,6 @@ class Solution {
                 ans2 += map2[i];
             }
         }
-        // System.out.println(ans1 + " " + ans2);
-
-        // for(int i=0; i<nums1.length; i++){
-        //     int ele =  nums1[i];
-        //     for(int j=0; j<nums2.length; j++){
-        //         if(nums2[j]==ele){
-        //             ans1++;
-        //             break;
-        //         }
-        //     }
-        // }
-
-        // for(int i=0; i<nums2.length; i++){
-        //     int ele =  nums2[i];
-        //     for(int j=0; j<nums1.length; j++){
-        //         if(nums1[j]==ele){
-        //             ans2++;
-        //             break;
-        //         }
-        //     }
-        // }
 
         return new int[] { ans1, ans2 };
     }
