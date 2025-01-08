600fc14a0e1bb81bb6406f2975a10e381621c4ce
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-08 15:18:08 +0530

    Time: 1 ms (91.55%) | Memory: 41.7 MB (69.35%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -5,9 +5,7 @@ class Solution {
 
         for (int i = 0; i < s.length() - 2; i++) {
             char ch1 = s.charAt(i), ch2 = s.charAt(i + 1), ch3 = s.charAt(i + 2);
-            if (ch1 == ch2 || ch1 == ch3 || ch2 == ch3) {
-                continue;
-            }else{
+            if (ch1 != ch2 && ch1 != ch3 && ch2 != ch3) {
                 count++;
             }
         }
