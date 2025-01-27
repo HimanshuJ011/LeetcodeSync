e7b0d93d55facd6e058a68ab3ef6614a599b4ed6
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-27 19:08:31 +0530

    Time: 5 ms (29.64%) | Memory: 42.8 MB (5.78%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()) return new ArrayList<>();
+        return pad("", digits);
+    }
+    private List<String> pad(String p, String up){
+        if(up.isEmpty()){
+          ArrayList<String> list = new ArrayList<>();
+          list.add(p);
+          return list;
+        }
+
+        int digit =  up.charAt(0) - '0';
+        ArrayList<String> list = new ArrayList<>();
+        int end = (digit*3)-3;
+        int start = (digit-2)*3;
+
+        if(digit == 7){
+            end = (digit*3)-3+1;
+        }
+        if(digit == 9 ){
+            start = (digit-2)*3+1;
+            end = (digit*3)-3+2;
+        }
+        if(digit == 8){
+          start = (digit-2)*3+1;
+          end = (digit*3)-3+1;
+
+        }
+
+        for(int i=start; i < end; i++){
+            char ch = (char) ('a'+ i);
+            list.addAll(pad(p+ch,up.substring(1)));
+        }
+        return list;
+
+    }
+}
