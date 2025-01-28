a2f7b850c7a4d0cd03042f47c50e56ea16d8c3fd
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-28 11:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (68.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -2,25 +2,44 @@ class Solution {
 
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        solve("", n, 0, 0, result);
+        // solve("", n, 0, 0, result);
+        solve2(new StringBuilder(), n, 0, 0, result);
         return result;
     }
 
-    private void solve(String s, int n, int open, int close, List<String> result) {
-        if (s.length() == 2 * n) {
-            result.add(s);
+    // private void solve(String s, int n, int open, int close, List<String> result) {
+    //     if (s.length() == 2 * n) {
+    //         result.add(s);
+    //         return;
+    //     }
+
+    //     if (open < n) {
+    //         s += '(';
+    //         solve(s, n, open + 1, close, result);
+    //         s = s.substring(0, s.length() - 1);
+    //     }
+    //     if (close < open) {
+    //         s += ')';
+    //         solve(s, n, open, close + 1, result);
+    //         s = s.substring(0, s.length() - 1);
+    //     }
+    // }
+
+    private void solve2(StringBuilder sb, int n, int open, int close, List<String> result) {
+        if (sb.length() == 2 * n) {
+            result.add(sb.toString());
             return;
         }
 
         if (open < n) {
-            s += '(';
-            solve(s, n, open + 1, close, result);
-            s = s.substring(0, s.length() - 1);
+            sb.append("(");
+            solve2(sb, n, open + 1, close, result);
+            sb.deleteCharAt(sb.length() - 1);
         }
         if (close < open) {
-            s += ')';
-            solve(s, n, open, close + 1, result);
-            s = s.substring(0, s.length() - 1);
+            sb.append(")");
+            solve2(sb, n, open, close + 1, result);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
