bdad3c1d3b3fc94d326285dcf8b12addfadeb17e
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-28 15:10:50 +0530

    Time: 5 ms (29.75%) | Memory: 42.7 MB (5.83%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,38 +1,40 @@
 class Solution {
+
     public List<String> letterCombinations(String digits) {
-        if(digits.isEmpty()) return new ArrayList<>();
+        ArrayList<String> ans = new ArrayList<>();
+        if (digits.length() == 0) return ans;
+
         return pad("", digits);
     }
-    private List<String> pad(String p, String up){
-        if(up.isEmpty()){
-          ArrayList<String> list = new ArrayList<>();
-          list.add(p);
-          return list;
+
+    private List<String> pad(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
 
-        int digit =  up.charAt(0) - '0';
+        int digit = up.charAt(0) - '0';
         ArrayList<String> list = new ArrayList<>();
-        int end = (digit*3)-3;
-        int start = (digit-2)*3;
+        int end = (digit * 3) - 3;
+        int start = (digit - 2) * 3;
 
-        if(digit == 7){
-            end = (digit*3)-3+1;
+        if (digit == 7) {
+            end = (digit * 3) - 3 + 1;
         }
-        if(digit == 9 ){
-            start = (digit-2)*3+1;
-            end = (digit*3)-3+2;
+        if (digit == 9) {
+            start = (digit - 2) * 3 + 1;
+            end = (digit * 3) - 3 + 2;
         }
-        if(digit == 8){
-          start = (digit-2)*3+1;
-          end = (digit*3)-3+1;
+        if (digit == 8) {
+            start = (digit - 2) * 3 + 1;
+            end = (digit * 3) - 3 + 1;
+        }
 
-        }
-
-        for(int i=start; i < end; i++){
-            char ch = (char) ('a'+ i);
-            list.addAll(pad(p+ch,up.substring(1)));
+        for (int i = start; i < end; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(pad(p + ch, up.substring(1)));
         }
         return list;
-
     }
 }
