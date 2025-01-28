58634e966019c6df76cc37375cdabf0a71b27e77
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-28 15:41:21 +0530

    Time: 5 ms (29.75%) | Memory: 42.5 MB (18.04%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,40 +1,25 @@
 class Solution {
 
     public List<String> letterCombinations(String digits) {
-        ArrayList<String> ans = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
         if (digits.length() == 0) return ans;
+        String[] mapping = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
-        return pad("", digits);
+        pad("", digits, mapping, ans);
+        return ans;
     }
 
-    private List<String> pad(String p, String up) {
+    private void pad(String p, String up, String[] mapping, List<String> ans) {
         if (up.isEmpty()) {
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
+            ans.add(p);
+            return;
         }
 
         int digit = up.charAt(0) - '0';
-        ArrayList<String> list = new ArrayList<>();
-        int end = (digit * 3) - 3;
-        int start = (digit - 2) * 3;
+        String chars = mapping[digit];
 
-        if (digit == 7) {
-            end = (digit * 3) - 3 + 1;
+        for (char ch : chars.toCharArray()) {
+            pad(p + ch, up.substring(1), mapping, ans);
         }
-        if (digit == 9) {
-            start = (digit - 2) * 3 + 1;
-            end = (digit * 3) - 3 + 2;
-        }
-        if (digit == 8) {
-            start = (digit - 2) * 3 + 1;
-            end = (digit * 3) - 3 + 1;
-        }
-
-        for (int i = start; i < end; i++) {
-            char ch = (char) ('a' + i);
-            list.addAll(pad(p + ch, up.substring(1)));
-        }
-        return list;
     }
 }
