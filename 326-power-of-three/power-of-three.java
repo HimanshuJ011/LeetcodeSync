c5a447429cf968288dd6fec108c4dea2bc2aeb5d
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-01-29 22:19:45 +0530

    Time: 8 ms (89.71%) | Memory: 44 MB (81.73%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,10 +1,15 @@
 class Solution {
 
     public boolean isPowerOfThree(int n) {
-        if (n == 1) return true;
-        if (n % 3 != 0 || n <= 0) return false;
-       
-        return (n % 3 == 0) && isPowerOfThree(n / 3);
+        // if (n == 1) return true;
+        // if (n % 3 != 0 || n <= 0) return false;
+        // return (n % 3 == 0) && isPowerOfThree(n / 3);
+        
+        if(n<=0) return false;
+        while(n%3==0){
+            n/=3;
+        }
+        return n==1;
      
     }
 }
