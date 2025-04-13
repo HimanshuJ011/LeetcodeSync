9270f948a4f1bce76584b2971eb8a9d286b0ce5f
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-04-13 16:06:01 +0530

    Time: 4 ms (72.27%) | Memory: 44.8 MB (28.38%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,22 +10,43 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        for(int i=0; i<lists.length; i++){
-            ListNode current = lists[i];
-            while (current != null) {
-                minHeap.add(current.val);
-                current = current.next;
-            }
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(
+        (a, b) -> a.val - b.val
+    );
+       for (ListNode list : lists) {
+        if (list != null) {
+            minHeap.add(list);
         }
-        ListNode head = new ListNode(0);
-        ListNode node = head;
+    }
+    ListNode dummy = new ListNode(0);
+    ListNode tail = dummy;
+     while (!minHeap.isEmpty()) {
+        ListNode minNode = minHeap.poll();
+        tail.next = minNode;
+        tail = tail.next;
 
-        while(!minHeap.isEmpty()){
-            node.next = new ListNode(minHeap.poll());
-            node = node.next;
+        if (minNode.next != null) {
+            minHeap.add(minNode.next);
         }
-        return head.next;
+    }
+
+    return dummy.next;
+
+        // for(int i=0; i<lists.length; i++){
+        //     ListNode current = lists[i];
+        //     while (current != null) {
+        //         minHeap.add(current.val);
+        //         current = current.next;
+        //     }
+        // }
+        // ListNode head = new ListNode(0);
+        // ListNode node = head;
+
+        // while(!minHeap.isEmpty()){
+        //     node.next = new ListNode(minHeap.poll());
+        //     node = node.next;
+        // }
+        // return head.next;
     }
 }
