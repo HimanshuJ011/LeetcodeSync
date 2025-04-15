d7bf5f68387001149d522c4c88daa39d0fb04a37
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-04-15 09:01:06 +0530

    Time: 26 ms (70.42%) | Memory: 57.4 MB (88.19%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -3,15 +3,18 @@ class Solution {
         Arrays.sort(players);
         Arrays.sort(trainers);
 
-        int l=0, r=0;
+        int l=0, r=0, ans=0;
 
         while(l<players.length && r<trainers.length){
             if(players[l] <= trainers[r]){
                 l++;
+                r++;
+                ans++;
+            }else{
+                r++;
             }
-            r++;
         }
-        return l;
+        return ans;
         
     }
 }
