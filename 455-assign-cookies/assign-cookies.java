fff4ae1f22efe95bb2fe2bffd11e016dfbc6ee53
Himanshu joshi <75946104+HimanshuJ011@users.noreply.github.com>
2025-04-15 09:01:55 +0530

    Time: 9 ms (99.52%) | Memory: 47.7 MB (99.67%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -3,15 +3,18 @@ class Solution {
         if(s.length == 0) return 0;
         Arrays.sort(s);
         Arrays.sort(g);
-        int l=0, r=0;
+        int l=0, r=0,count=0;
 
         while(l < s.length && r < g.length){
             if(g[r]<=s[l]){
                 r++;
+                count++;
+                l++;
+            }else{
+                l++;
             }
-            l++;
         }
      
-        return r;
+        return count;
     }
 }
